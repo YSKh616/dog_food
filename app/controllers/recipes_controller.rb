e5845a4c818891790e2911b69570f202072be39d
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-27 21:57:15 +0900

    set_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: :index
 
   def index
+  end
+
+  def show
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
