a95877546f5969845e6b006bc4f96a35b388495a
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-08-01 14:31:49 +0900

    set_user_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many: dogs
-  has_many: recipes
+  has_many :dogs
+  has_many :recipes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
