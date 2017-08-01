272708464aa1a5b18b68763355379aebbfaa0ab3
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-08-01 12:09:41 +0900

    fix_top_page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727052112) do
+ActiveRecord::Schema.define(version: 20170727131112) do
+
+  create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title", null: false
+    t.text "image"
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
