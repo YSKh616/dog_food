6f75b5f7abb20bbdf623d61c413089135f9536c1
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-08-26 01:22:07 +0900

    set_environment_for_favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_favicon
+    if Rails.env.production?
+      favicon_link_tag '/favicon.ico'
+    else
+      favicon_link_tag "/favicon-#{Rails.env}.ico"
+  end
+end
 end
