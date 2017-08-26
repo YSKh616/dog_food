818b49072e40c27ac2fd93f0e47457e9b337039b
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-08-27 01:37:24 +0900

    pett_add_function

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -4,7 +4,17 @@ class DogsController < ApplicationController
   end
 
   def create
-    @dog = Dog.new
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to '/users/current_user.id'
+    else
+      flash.now[:alert] = '未入力の項目があります'
+      render :new
+    end
   end
 
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :age, :type, :image, :sex, :allergy, :medicalhistory).merge(user_id: current_user.id)
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,5 @@
 class Dog < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  self.inheritance_column = :_type_disabled # この行を追加
 end
